4c0c99522119a26fbb72b981098efc430b937ac3
leejaejae <leesengjie@naver.com>
2024-07-31 15:45:34 +0900

    bodyParser, nodemon 적용

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
+const bodyParser = require('body-parser');  
+const { User } = require('./models/User');  
+
+app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://leesengjie:abcd1234@bolier-plate.unathks.mongodb.net/?retryWrites=true&w=majority&appName=bolier-plate', {})
@@ -9,8 +14,22 @@ mongoose.connect('mongodb+srv://leesengjie:abcd1234@bolier-plate.unathks.mongodb
 
 
 app.get('/', (req, res) => {  
-  res.send('node.js 공부 시작!')
+  res.send('안녕하세요. jaejae입니다.')
 })
+
+
+app.post('/register', async (req, res) => {
+  const user = new User(req.body)
+
+  const result = await user.save().then(()=>{
+    res.status(200).json({
+      success: true
+    })
+  }).catch((err)=>{
+    res.json({ success: false, err })
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
