422e36a25e4b763c792e5811df87019832b44e4d
leejaejae <leesengjie@naver.com>
2024-08-07 13:15:14 +0900

    react-router-dom 적용

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
+import LandingPage from "./views/LandingPage/LandingPage";
+import LoginPage from "./views/LoginPage/LoginPage";
+import RegisterPage from "./views/RegisterPage/RegisterPage";
 
 function App() {
   return (
     <div>
-      AppAppApp
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
